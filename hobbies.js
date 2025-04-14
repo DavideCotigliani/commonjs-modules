75303a77c5e115711ca27cc3717feb4432e4f377
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-14 14:33:19 +0200

    [UPD] exported the second function

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+function hobbies (hobbyOne,hobbyTwo,hobbyThree){
+    const hobbiesResult = [hobbyOne,hobbyTwo,hobbyThree]
+    return hobbiesResult
+}
+
+//esporto la funzione
+module.exports = hobbies;
