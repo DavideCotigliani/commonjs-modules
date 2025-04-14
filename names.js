9258e3f45ddfbcf9dde95c07d1035e8e8cfaa58c
DavideCotigliani <davide.cotigliani@gmail.com>
2025-04-14 14:28:10 +0200

    first commit

---
+++ names.js
@@ -0,0 +1,10 @@
+function names (firstName, lastName){
+    const namesResult = {
+        firstName:firstName,
+        lastName:lastName
+    }
+return namesResult
+}
+
+//esporto la funzione del file
+module.exports = names;
